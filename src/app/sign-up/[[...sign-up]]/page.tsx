b36a15b978aff44c956c47a3efcc638cedab1976
unknown <hamzaalhaj987@gmail.com>
2024-07-05 14:14:09 +0300

    minor fixes

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="h-[calc(100vh-96px)] flex items-center justify-center">
+    <div className="h-full flex items-center justify-center">
       <SignUp />
     </div>
   );
